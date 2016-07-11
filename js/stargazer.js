a44b214d39957974504b5a983c7c6ebecc34088a
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-07-11 18:53:12 -0500

    finish homework

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,28 @@
+$(document).ready(function() {
+
+
+$('#search').on('submit', function(event) {
+  event.preventDefault();
+  var starSearch = $('#search-name').val();
+  search(starSearch);
+  });
+})
+
+function showImage(json) {
+    var image = json.image.src
+    $('#preview').html(`<div><img src='${image}'</div>`)
+};
+
+
 function search(name) {
+
   var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
+
+  $.ajax({
+    method: "GET",
+    url: api,
+    dataType: 'jsonp',
+    jsonpCallback: "showImage"
+  })
+
 }
